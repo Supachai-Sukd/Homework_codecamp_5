c998bd96c23d0c3e8846fb18add59717a65bd89d
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-03-29 20:32:13 +0700

    Loop 5

--- CodeCamp5/Javascript Part4/แบบฝึกหัด 1.9/loop.js
+++ CodeCamp5/Javascript Part4/แบบฝึกหัด 1.9/loop.js
@@ -20,8 +20,31 @@
 //     alert(i);
 // }
 
-let i = 3;
+// let i = 3;
 
-while (i) {
-  alert( i-- );
-}
+// while (i) {
+//   alert( i-- );
+// }
+
+
+// let i = 0;
+// while (++i < 5) alert( i );
+
+// let i = 0;
+// while (i++ < 5) alert( i );
+
+// for (let i = 0; i < 5; i++) alert( i );  //0 1 2 3 4
+
+// for (let i = 0; i < 5; ++i) alert( i ); //0 1 2 3 4
+
+// for(let i = 2;i <= 10;i++){
+//     alert(`Number ${i}`);
+// }
+
+let i = 0;
+ while(i++ < 3) alert(`Number ${i-1}!`)
+
+// for (let i = 0; i < 3; i++) {
+//     alert( `number ${i}!` );
+//   }
+  
