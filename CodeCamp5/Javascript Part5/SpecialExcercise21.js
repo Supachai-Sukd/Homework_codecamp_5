e73358e34f41d482e98bf6af4eae4c1ac9612c87
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-04-04 09:25:56 +0700

    Special Ex

--- CodeCamp5/Javascript Part5/SpecialExcercise21.js
+++ CodeCamp5/Javascript Part5/SpecialExcercise21.js
@@ -1,9 +1,21 @@
 function method(n){
     let i,j,step="",col=(n*2)-1,row=n;
-    for(i =1;i <= n;i++){
+    for(i =n;i > 0;i--){
         for(j =1;j <= col;j++){
-            
+            if(j <= n){
+                j < i ? (step += "-") : (step += "*")
+                
+            }
+            else{
+                j > col - i + 1 ? (step += "-") : (step += "*") 
+            }
         }
         step += "\n"
     }
+    console.log(step);
+    
 }
+
+method(2)
+method(3)
+method(4)
