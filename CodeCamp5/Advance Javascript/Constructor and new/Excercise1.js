86cfa4aa0f8a423671b3c640913e010bb720248f
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-04-05 22:24:22 +0700

    Constructor Excercise 1

---
+++ CodeCamp5/Advance Javascript/Constructor and new/Excercise1.js
@@ -0,0 +1,105 @@
+// function Calculator () {
+//     this.read = function () {
+//         this.number1 = Number(prompt("Enter first number"));
+//         this.number2 = Number(prompt("Enter second number"));
+//     }
+//     this.sum = function () {
+//         return this.number1 + this.number2
+//     }
+//     this.mul = function () {
+//         return this.number1 * this.number2
+//     }
+// }
+
+
+
+// let calc = new Calculator();
+
+
+
+
+// function Calculator () {
+//     this.read = function () {
+//         this.number1 = Number(prompt("Enter first number"))
+//         this.number2 = Number(prompt("Enter second number"))
+//     }
+//     this.mul = function () {
+//         return this.number1 + this.number2
+//     }
+//     this.sum = function () {
+//         return this.number1 * this.number2
+//     }
+// }
+
+// let calc = new Calculator()
+
+
+
+
+
+
+
+
+// function Calculator () {
+//     this.read = function () {
+//         this.number1 = Number(prompt("Enter first number"))
+//         this.number2 = Number(prompt("Enter second number"))
+//     }
+//     this.sum = function () {
+//         return this.number1 + this.number2
+//     }
+//     this.mul = function () {
+//         return this.number1 * this.number2
+//     }
+// }
+
+// let calc = new Calculator()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Calculator () {
+//     this.read = function () {
+//         this.num1 = Number(prompt("Enter first number"))
+//         this.num2 = Number(prompt(`Enter second number`))
+//     }
+//     this.sum = function () {
+//         return this.num1 + this.num2;
+//     }
+//     this.mul = function () {
+//         return this.num1 * this.num2
+//     }
+// }
+
+// let calc = new Calculator()
+
+
+
+
+
+
+
+
+function Salculator () {
+    this.read = function () {
+        this.num1 = Number(prompt(`Enter first number`));
+        this.num2 = Number(prompt(`Enter second number`));
+    }
+    this.sum =function () {
+        return this.num1 + this.num2;
+    }
+    this.mul = function () {
+        return this.num1 * this.num2;
+    }
+}
+
+let calc = new Salculator()
