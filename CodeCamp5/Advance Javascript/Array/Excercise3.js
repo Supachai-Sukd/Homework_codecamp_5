eee6b77f1602965da6780bce8dd0385658712481
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-04-06 22:44:57 +0700

    Almost done

---
+++ CodeCamp5/Advance Javascript/Array/Excercise3.js
@@ -0,0 +1,10 @@
+function sumInput (sum) {
+    let i,result;
+    let total = [];
+    while (sum !== typeof(Number)) {
+        let read = prompt("Enter number");
+        read = total[i]
+        i++;
+    }
+    alert(result)
+}
