cf133951885f34eeb25f967216ec0a171afa7488
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-04-11 20:28:19 +0700

    Anagram

--- CodeCamp5/Advance Javascript/Method of Array/MapAndSet2.js
+++ CodeCamp5/Advance Javascript/Method of Array/MapAndSet2.js
@@ -1,6 +1,6 @@
 function aclean(arr) {
   let result = new Map()
-
+    let anagramTeeruk;
   for (let str of arr) {
       let sorted = str.split('').sort().join('')
       if (result.has(sorted)) {
@@ -9,7 +9,19 @@ function aclean(arr) {
           result.set(sorted, [str])
       }
   }
-  return Array.from(result.values())
+  let resultArr = Array.from(result.values())
+  let obj1 = resultArr[0]
+  console.log(obj1);
+  let obj2 = resultArr[1]
+  console.log(obj2);
+  let obj3 = resultArr[2]
+  console.log(obj3);
+  let obj4 = resultArr[3]
+  console.log(obj4);
+  
+  anagramTeeruk = (`${obj1} ${obj2[0]} ${obj4[0]}`)
+  
+  return anagramTeeruk;
 }
 let array = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 console.log(aclean(array));
