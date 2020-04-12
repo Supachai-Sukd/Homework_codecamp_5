e2e489cd2150ea61aa664fa33b7f2a40286bcb91
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-04-12 22:27:23 +0700

    Traing today

---
+++ CodeCamp5/Advance Javascript/Method of Array/Training Room/Train7.js
@@ -0,0 +1,11 @@
+let array1 = [
+    { name: "apple", surname: "London" },
+    { name: "banana", surname: "Bangkok" },
+    { name: "watermelon", surname: "Singapore" },
+  ]
+
+let array2 = array1.map(function(item, index, array) {
+    return item.name + " " + item.surname;
+})
+
+console.log(array2);
---
+++ CodeCamp5/Advance Javascript/Method of Array/Training Room/Train8.js
@@ -0,0 +1,11 @@
+let array1 = [1,3,4,5,6,7,8];
+let array2 = array1.map(function(item, index, array) {
+    if (item % 2 == 0) {
+        item = "even"
+    } else{
+        item = "odd"
+    }
+    return item;
+})
+
+console.log(array2);
