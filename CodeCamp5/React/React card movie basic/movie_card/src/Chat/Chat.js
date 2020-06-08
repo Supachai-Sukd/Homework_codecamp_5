c2191603995a9d3dd055e6c82152d0810fb2d91f
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-06-08 21:56:36 +0700

    App chat day 2

--- CodeCamp5/React/React card movie basic/movie_card/src/Chat/Chat.js
+++ CodeCamp5/React/React card movie basic/movie_card/src/Chat/Chat.js
@@ -5,22 +5,24 @@ export default class Chat extends Component {
         list: [],
         text: "",
     }
+    add = () => {
+      let list = [...this.state.list, this.state.text]
+      this.setState({list : list})
+      this.setState({text: ""})
+    }
   render() {
     return (
       <div style={{margin: "40px auto",height:"300px",width:"300px",backgroundColor:"whitesmoke"}}>
         <div style={{height: "250px"}}>
             <ul style={{listStyle: "none"}}>
-                <li>aaaaaaaa</li>
-                <li>aaaaaaaa</li>
-                <li>aaaaaaaa</li>
-                <li>aaaaaaaa</li>
-                <li>aaaaaaaa</li>
-                <li>aaaaaaaa</li>
+               
+    {this.state.list.map( str => <li>{str}</li> )}
             </ul>
         </div>
         <div >
-            <input type="text" placeholder="message" value={this.state.text} onChange={(e) => console.log({event: e})}></input>
-            <button>send</button>
+            <input type="text" placeholder="message" value={this.state.text}
+            onChange={(e) => this.setState({text: e.target.value})}></input>
+            <button onClick={this.state.text.length > 0? this.add: null}>send</button>
         </div>
       </div>
     );
