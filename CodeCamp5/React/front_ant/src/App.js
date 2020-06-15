6de36f6ac8b6b608232426992aabc041c4e7f44b
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-06-15 23:53:38 +0700

    Ant validation

--- CodeCamp5/React/front_ant/src/App.js
+++ CodeCamp5/React/front_ant/src/App.js
@@ -1,76 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Table,Row,Col} from "antd"
+import {Row,Col,Form,Input} from "antd"
+
 
 function App() {
-  const student = [{
-    "first_name": "Nona",
-    "last_name": "St. Hill",
-    "email": "nsthill0@miitbeian.gov.cn",
-    "gender": "Female",
-    "university": "Istanbul Arel University"
-  }, {
-    "first_name": "Felice",
-    "last_name": "Wilcocke",
-    "email": "fwilcocke1@nytimes.com",
-    "gender": "Male",
-    "university": "University of Maryland Baltimore County"
-  }, {
-    "first_name": "Chester",
-    "last_name": "Ruske",
-    "email": "cruske2@spotify.com",
-    "gender": "Male",
-    "university": "Rafsanjan University of Medical Sciences"
-  }, {
-    "first_name": "Christye",
-    "last_name": "Biddy",
-    "email": "cbiddy3@sakura.ne.jp",
-    "gender": "Female",
-    "university": "Universidad Cientifica Latinoamericana de Hidalgo"
-  }, {
-    "first_name": "Naoma",
-    "last_name": "Bithell",
-    "email": "nbithell4@is.gd",
-    "gender": "Female",
-    "university": "Kashan University of Medical Sciences"
-  }, {
-    "first_name": "Adore",
-    "last_name": "Spaldin",
-    "email": "aspaldin5@wunderground.com",
-    "gender": "Female",
-    "university": "Canisius College"
-  }]
-
-const columns = [
-  {
-    title: "เมลล์",
-    dataIndex: "email"
-  },
-  {
-    title: "เพศ",
-    dataIndex: "gender"
-  },
-{
-  title: "ชื่อจริง",
-  dataIndex: "first_name"
-},
-{
-  title: "นามสกุล",
-  dataIndex: "last_name"
-},
-{
-  title: "มหาลัย",
-  dataIndex: "university"
-}
-
-]
-
-  return (
+  const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  }
+  
+return (
     <div className="App">
       <Row justify="center">
         <Col xs={24} sm={24} md={20} lg={16} xl={14} xxl={12}>
-        <Table bordered={true} columns={columns} dataSource={student} size="small" />
+        <Form {...layout}>
+          <Form.Item label="name">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="Phone">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="email">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="password">
+            <Input/>
+          </Form.Item>
+        </Form>
         </Col>
        
       </Row>
