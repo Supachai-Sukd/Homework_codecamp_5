e83b3ed333f71acd892224205f578cc2c8cc44de
Supachai-Sukd <53645737+Supachai-Sukd@users.noreply.github.com>
2020-06-22 22:03:49 +0700

    Create both manager and branch

--- CodeCamp5/Sequelize/setup_project/controllers/Branch.js
+++ CodeCamp5/Sequelize/setup_project/controllers/Branch.js
@@ -4,13 +4,24 @@ const { sequelize, Sequelize } = require('../models')
 const Op = Sequelize.Op
 
 const getAllBranchs = async (req, res) => {
-    const allBranchs = await db.Branch.findAll()
+    const allBranchs = await db.Branch.findAll({ include: db.Manager })
     res.status(200).send(allBranchs)
 }
 
 const createNewBranchsAndManager = async (req, res) => {
     const { managerName, managerExperience } = req.body
     const { branchName, branchSize } = req.body
+    const newManager = await db.Manager.create( {
+        name: managerName,
+        experience: managerExperience
+    } )
+    const newBranch = await db.Branch.create( {
+        name: branchName,
+        size: branchSize,
+        manager_id: newManager.id
+    } )
+
+    res.status(201).send(newBranch)
 }
 
 module.exports = {
